441b0569d50a254e7b88cbae2986f9537151b482
danielfalqui <100230158+danielfalqui@users.noreply.github.com>
2023-09-14 18:12:56 -0300

    Add files via upload

--- obj_trk_final.py
+++ obj_trk_final.py
@@ -21,8 +21,8 @@ bbox = cv2.selectROI("tracking",img,False)
 # Inicialize o rastreador em img e na caixa delimitadora
 tracker.init(img,bbox)
 
-def goal_track(img,bbox):
-    x,y,w,h = int(bbox[0]),int(bbox[1]),int(bbox[2]),int(bbox[3])
+def goal_track(img,bbox): 
+    x,y,  w, h = int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3])
     c1 = x + int(w/2)
     c2 = y + int(h/2)
     cv2.circle(img,(c1,c2),2,(0,0,255),5)
@@ -31,31 +31,27 @@ def goal_track(img,bbox):
     dist = math.sqrt(((c1-p1)**2) + (c2-p2)**2)
     print(dist)
 
-    if(dist<=20):
+    if(dist<=20): 
         cv2.putText(img,"Ponto",(300,90),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,255,0),2)
 
     xs.append(c1)
     ys.append(c2)
 
-    for i in range(len(xs)-1):
+    for i in range(len(xs)-1): 
         cv2.circle(img,(xs[i],ys[i]),2,(0,0,255),5)
 
-def drawBox(img,bbox):
-    x,y,w,h = int(bbox[0]),int(bbox[1]),int(bbox[2]),int(bbox[3])
+def drawBox(img,bbox): 
+    x,y, w, h = int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3])
     cv2.rectangle(img,(x,y),((x+w),(y+h)),(255,0,255),3,1)
     cv2.putText(img,"Rastreando",(75,90),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,255,0),2)
 
-while True:
+while True: 
     check,img = video.read()
-    succes,bbox = tracker.update(img)
-    pass
+    success,bbox = tracker.update(img)
 
-if succes:
-    drawBox(img, bbox)
-else:
-    cv2.putText(img, "Errou", (75,90), cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
+    if success: 
+        drawBox(img, bbox)
+    else: 
+        cv2.putText(img, "Errou", (75,90), cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
 
-video.release()
-cv2.destroyALLwindows() 
-
-goal_track(img,bbox)
+    goal_track(img,bbox)
